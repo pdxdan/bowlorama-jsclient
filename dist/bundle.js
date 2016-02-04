9290f7f46c31ba33105d5157d35b2bb2477f5169
Daniel Anolik <dan@anolik.net>
2016-02-04 15:58:45 -0800

    updated configs

--- dist/bundle.js
+++ dist/bundle.js
@@ -29851,11 +29851,11 @@
 	exports.default = {
 	    gameid: new Date().getTime(),
 	    players: {
-	        "aron": {
+	        "Player1": {
 	            frames: [],
 	            score: 0
 	        },
-	        "dan": {
+	        "Player2": {
 	            frames: [],
 	            score: 0
 	        }
@@ -33167,7 +33167,8 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	var apiHostPort = exports.apiHostPort = 'c6rt4qktca.execute-api.us-east-1.amazonaws.com';
+	//export const apiHostPort = 'c6rt4qktca.execute-api.us-east-1.amazonaws.com';  //deprecated east coast location
+	var apiHostPort = exports.apiHostPort = 'oarmnm6fg3.execute-api.us-west-2.amazonaws.com'; // hip new PNW location
 	//export const apiHostPort = 'localhost:8080';
 	var apiRestProtocol = exports.apiRestProtocol = 'https';
 
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const apiHostPort = 'c6rt4qktca.execute-api.us-east-1.amazonaws.com';
+//export const apiHostPort = 'c6rt4qktca.execute-api.us-east-1.amazonaws.com';  //deprecated east coast location
+export const apiHostPort = 'oarmnm6fg3.execute-api.us-west-2.amazonaws.com';  // hip new PNW location
 //export const apiHostPort = 'localhost:8080';
 export const apiRestProtocol = 'https';
